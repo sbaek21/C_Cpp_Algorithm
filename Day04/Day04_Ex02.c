e6f593f794348dca74317107c493082151f601bc
sbaek21 <62567659+sbaek21@users.noreply.github.com>
2022-08-01 09:01:24 +0900

    Add files via upload

---
+++ Day04/Day04_Ex02.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//
+//int main(void) {
+//	//int num1, num2, maxNum; 각각 입력
+//	//num1 - num2 가 0보다 크면
+//	//여기선 출력 X, maxNum에 값 넣은거.
+//	//num1 - num2 가 0보다 작으면
+//	//여기선 출력 X, maxNum에 값 넣은거
+//	//출력은 if, else 빠져나와서 maxNum 출력
+//
+//	int num1, num2, maxNum;
+//
+//	scanf_s("%d %d", &num1, &num2);
+//
+//	if (num1 - num2 > 0) {
+//		maxNum = num1;
+//	}
+//	else { // num1 - num2 < 0
+//		maxNum = num2;
+//	}
+//	printf("%d과 %d중 큰 수는 %d입니다.",
+//		num1, num2, maxNum);
+//}
